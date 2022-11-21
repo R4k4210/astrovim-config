900d2f976937f22702d2a50f7ba8625c2e24dcf8
R4k4210 <elchopi26@gmail.com>
2022-11-21 00:34:46 -0300

    Small changes

--- highlights.lua
+++ highlights.lua
@@ -1,13 +1,29 @@
 local highlights = {
-  -- duskfox = { -- a table of overrides/changes to the default
-  --   Normal = { bg = "#000000" },
-  -- },
-  default_theme = function(highlights) -- or a function that returns a new table of colors to set
-    local C = require "default_theme.colors"
 
-    highlights.Normal = { fg = C.fg, bg = C.bg }
-    return highlights
-  end,
+  init = function()
+    -- get highlights from highlight groups
+    local normal = astronvim.get_hlgroup "Normal"
+    local fg, bg = normal.fg, "#1d242e" --normal.bg
+    local bg_alt = astronvim.get_hlgroup("Visual").bg
+    local green = astronvim.get_hlgroup("String").fg
+    local red = astronvim.get_hlgroup("Error").fg
+    -- return a table of highlights for telescope based on colors gotten from highlight groups
+    return {
+      TelescopeBorder = { fg = bg_alt, bg = bg },
+      TelescopeNormal = { bg = bg },
+      TelescopePreviewBorder = { fg = bg, bg = bg },
+      TelescopePreviewNormal = { bg = bg },
+      TelescopePreviewTitle = { fg = bg, bg = green },
+      TelescopePromptBorder = { fg = bg_alt, bg = bg_alt },
+      TelescopePromptNormal = { fg = fg, bg = bg_alt },
+      TelescopePromptPrefix = { fg = red, bg = bg_alt },
+      TelescopePromptTitle = { fg = bg, bg = red },
+      TelescopeResultsBorder = { fg = bg, bg = bg },
+      TelescopeResultsNormal = { bg = bg },
+      TelescopeResultsTitle = { fg = bg, bg = bg },
+    }
+
+  end
 }
 
 return highlights
